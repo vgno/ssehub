299cdedebc71c513e7ee241c7cac7a2b57f1065b
Sindre Gulseth <sgulseth@gmail.com>
2015-08-12 08:24:19 -0400

    Added redis cache methods

--- src/CacheAdapters/Redis.cpp
+++ src/CacheAdapters/Redis.cpp
@@ -35,7 +35,7 @@ void Redis::Connect() {
 }
 
 void Redis::Disconnect() {
-
+  // TODO: Gracefully handle disconnects
 }
 
 void Redis::Reconnect(int delay) {
@@ -45,10 +45,25 @@ void Redis::Reconnect(int delay) {
 }
 
 void Redis::CacheEvent(SSEEvent* event) {
-  RedisValue result;
-  result = _client->command("ZADD", _key, event->getid(), event->get());
+  RedisValue result = _client->command("HSET", _key, event->getid(), event->get());
   if (result.isError()) {
     LOG(ERROR) << "SET error: " << result.toString();
+  }
+
+  result = _client->command("HLEN", _key);
+  if (result.isError()) {
+    LOG(ERROR) << "HLEN error" << result.toString();
+  }
+  if (result.isOk()) {
+    if (result.toInt() > _config.length) {
+      result = _client->command("HKEYS", _key);
+      if (result.isError()) {
+        LOG(ERROR) << "HKEYS error: " << result.toString();
+      }
+      if (result.isOk()) {
+        _client->command("HDEL", _key, result.toArray().front().toString());
+      }
+    }
   }
 }
 
@@ -56,21 +71,70 @@ deque<string> Redis::GetEventsSinceId(string lastId) {
   deque<string> events;
   RedisValue result;
 
-  result = _client->command("ZRANGE", _key, lastId, "-1");
+  result = _client->command("HGETALL", _key);
   if (result.isError()) {
-    LOG(ERROR) << "SET error: " << result.toString();
+    LOG(ERROR) << "GET error: " << result.toString();
   }
-  
 
+  if (result.isOk() && result.isArray()) {
+    std::vector<RedisValue> resultArray = result.toArray();
+    string id = "";
+    string data = "";
+    while (!resultArray.empty()) {
+      RedisValue eventResult = resultArray.back();
+      if (eventResult.isString()) {
+        if (data == "") {
+          data = eventResult.toString();
+        } else {
+          id = eventResult.toString();
+          if (id >= lastId) {
+            events.push_front(data);
+          }
+          id = "";
+          data = "";
+        }
+      }
+      resultArray.pop_back();
+    }
+  }
   return events;
 }
 
 deque<string> Redis::GetAllEvents() {
   deque<string> events;
 
+  RedisValue result = _client->command("HGETALL", _key);
+  if (result.isError()) {
+    LOG(ERROR) << "GET error: " << result.toString();
+  }
+
+  if (result.isOk() && result.isArray()) {
+    std::vector<RedisValue> resultArray = result.toArray();
+    string data = "";
+    while (!resultArray.empty()) {
+      RedisValue eventResult = resultArray.back();
+      if (eventResult.isString()) {
+        if (data == "") {
+          data = eventResult.toString();
+        } else {
+          events.push_front(data);
+          data = "";
+        }
+      }
+      resultArray.pop_back();
+    }
+  }
   return events;
 }
 
 int Redis::GetSizeOfCachedEvents() {
-    return (int) 0;
+  int size = 0;
+  RedisValue result = _client->command("HLEN", _key);
+  if (result.isError()) {
+    LOG(ERROR) << "GET error: " << result.toString();
+  }
+  if (result.isOk()) {
+    size = result.toInt();
+  }
+  return size;
 }
