7c705c89c82e25c42305f613e06918aa3148f6b6
Ole Fredrik Skudsvik <ole.skudsvik@gmail.com>
2015-11-06 15:00:10 +0100

    Use one connection to redis per cache request.

--- src/CacheAdapters/Redis.cpp
+++ src/CacheAdapters/Redis.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/ip/address.hpp>
+#include <boost/foreach.hpp>
 
 using namespace std;
 extern int stop;
@@ -19,13 +20,10 @@ Redis::Redis(const string key, const ChannelConfig& config) : _config(config), _
   if (_host.empty()) {
     LOG(ERROR) << "Failed to look up host for redis adapter " << _config.server->GetValue("redis.host") << " Retrying in 5 seconds.";
   }
-
-  GetClient();
 }
 
-RedisSyncClient* Redis::GetClient() {
+bool Redis::InitClient(RedisSyncClient& client)  {
   boost::asio::ip::address address;
-  boost::asio::io_service ioService;
   string errmsg;
 
   try {
@@ -34,26 +32,25 @@ RedisSyncClient* Redis::GetClient() {
     LOG(ERROR) << "Boost address lookup error: " << error.what();
   }
 
-  RedisSyncClient* client = new RedisSyncClient(ioService);
-
-  if(!client->connect(address, _port, errmsg)) {
+  if(!client.connect(address, _port, errmsg)) {
     LOG(ERROR) << "Failed to connect to redis: " << errmsg << ". Host: " << _host << " Port: " << _port;
-
-    return NULL;
+    return false;
   }
-  return client;
+
+  return true;
 }
 
 void Redis::CacheEvent(SSEEvent* event) {
   RedisValue result;
+  boost::asio::io_service ioService;
+  RedisSyncClient client(ioService);
 
-  RedisSyncClient* client = GetClient();
-  if (!client) {
+  if (!InitClient(client)) {
     return;
   }
 
   try {
-    result = client->command("HSET", _key, event->getid(), event->get());
+    result = client.command("HSET", _key, event->getid(), event->get());
   } catch (const runtime_error& error) {
     LOG(ERROR) << "Redis::CacheEvent: " << error.what();
   }
@@ -62,18 +59,18 @@ void Redis::CacheEvent(SSEEvent* event) {
     LOG(ERROR) << "SET error: " << result.toString();
   }
 
-  result = client->command("HLEN", _key);
+  result = client.command("HLEN", _key);
   if (result.isError()) {
     LOG(ERROR) << "HLEN error" << result.toString();
   }
   if (result.isOk()) {
     if (result.toInt() > _config.cacheLength) {
-      result = client->command("HKEYS", _key);
+      result = client.command("HKEYS", _key);
       if (result.isError()) {
         LOG(ERROR) << "HKEYS error: " << result.toString();
       }
       if (result.isOk()) {
-        client->command("HDEL", _key, result.toArray().front().toString());
+        client.command("HDEL", _key, result.toArray().front().toString());
       }
     }
   }
@@ -82,14 +79,15 @@ void Redis::CacheEvent(SSEEvent* event) {
 deque<string> Redis::GetEventsSinceId(string lastId) {
   deque<string> events;
   RedisValue result;
+  boost::asio::io_service ioService;
+  RedisSyncClient client(ioService);
 
-  RedisSyncClient* client = GetClient();
-  if (!client) {
+  if (!InitClient(client)) {
     return events;
   }
 
   try {
-    result = client->command("HGETALL", _key);
+    result = client.command("HGETALL", _key);
   } catch (const runtime_error& error) {
     LOG(ERROR) << "Redis::GetEventsSinceId: " << error.what();
   }
@@ -100,39 +98,39 @@ deque<string> Redis::GetEventsSinceId(string lastId) {
 
   if (result.isOk() && result.isArray()) {
     std::vector<RedisValue> resultArray = result.toArray();
-    string id = "";
-    string data = "";
-    while (!resultArray.empty()) {
-      RedisValue eventResult = resultArray.back();
-      if (eventResult.isString()) {
-        if (data == "") {
-          data = eventResult.toString();
-        } else {
-          id = eventResult.toString();
-          if (id >= lastId) {
-            events.push_front(data);
+
+    bool isId = true;
+    bool ignoreEvent = true;
+    BOOST_FOREACH(const RedisValue& value, resultArray) {
+      if (value.isString() && value.toString().length() > 0) {
+        if (isId) {
+          if (value.toString().compare(lastId) == 0) {
+            ignoreEvent = false;
           }
-          id = "";
-          data = "";
+        } else if (!ignoreEvent){
+          events.push_back(value.toString());
         }
+
+        isId = !isId;
       }
-      resultArray.pop_back();
     }
   }
+
   return events;
 }
 
 deque<string> Redis::GetAllEvents() {
   RedisValue result;
   deque<string> events;
+  boost::asio::io_service ioService;
+  RedisSyncClient client(ioService);
 
-  RedisSyncClient* client = GetClient();
-  if (!client) {
+  if (!InitClient(client)) {
     return events;
   }
 
   try {
-    result = client->command("HGETALL", _key);
+    result = client.command("HGETALL", _key);
   } catch (const runtime_error& error) {
     LOG(ERROR) << "Redis::GetEventsSinceId: " << error.what();
   }
@@ -143,34 +141,35 @@ deque<string> Redis::GetAllEvents() {
 
   if (result.isOk() && result.isArray()) {
     std::vector<RedisValue> resultArray = result.toArray();
-    string data = "";
-    while (!resultArray.empty()) {
-      RedisValue eventResult = resultArray.back();
-      if (eventResult.isString()) {
-        if (data == "") {
-          data = eventResult.toString();
-        } else {
-          events.push_front(data);
-          data = "";
+
+    bool isId = true;
+    BOOST_FOREACH(const RedisValue& value, resultArray) {
+      if (value.isString() && value.toString().length() > 0) {
+        if (!isId) {
+          string data = value.toString();
+          events.push_back(data);
         }
+
+        isId = !isId;
       }
-      resultArray.pop_back();
     }
   }
+
   return events;
 }
 
 int Redis::GetSizeOfCachedEvents() {
   int size = 0;
   RedisValue result;
+  boost::asio::io_service ioService;
+  RedisSyncClient client(ioService);
 
-  RedisSyncClient* client = GetClient();
-  if (!client) {
+  if (!InitClient(client)) {
     return size;
   }
 
   try {
-    result = client->command("HLEN", _key);
+    result = client.command("HLEN", _key);
   } catch (const runtime_error& error) {
     LOG(ERROR) << "Redis::GetEventsSinceId: " << error.what();
   }
@@ -188,9 +187,11 @@ int Redis::GetSizeOfCachedEvents() {
 
 string Redis::Lookup(string hostname) {
   hostent * record = gethostbyname(hostname.c_str());
+
   if(record == NULL) {
     return "";
   }
+
   in_addr * address = (in_addr * )record->h_addr;
   string ip_address = inet_ntoa(* address);
 
